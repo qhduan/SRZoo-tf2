9e853d9e617e9243b92f76d87ee77898536de994
qhduan <mail@qhduan.com>
2020-06-05 20:35:06 +0800

    add browser mode

---
+++ convert/convert_js.sh
@@ -0,0 +1,3 @@
+
+# user tensorflow convereter convert tf2saved to tfjs
+tensorflowjs_converter ./carn_x2 ./carn_x2_js
---
+++ doc/precache-manifest.c6fb1da3947cc798b1302b6e7a5d13eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c571549803ce0522da3b37952f3604bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a9a258e269add78c27",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "86e60c76188efc63220e",
+    "url": "/static/js/2.34b1083b.chunk.js"
+  },
+  {
+    "revision": "9f3ece4be77bc30b04966fe84f4cf2e7",
+    "url": "/static/js/2.34b1083b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a9a258e269add78c27",
+    "url": "/static/js/main.70576ab9.chunk.js"
+  },
+  {
+    "revision": "33e6a543e7b4a23d1b50",
+    "url": "/static/js/runtime-main.acc8740a.js"
+  }
+]);
